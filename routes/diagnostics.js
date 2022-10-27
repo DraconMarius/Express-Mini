eb26184387f73414ccb3f34e85852eae01185951
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-10-27 11:31:28 -0700

    starter diagnostic routes

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -1,10 +1,26 @@
 const diagnostics = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+let diagnosticJSON = require('../db/diagnostics.json');
+console.log(typeof diagnosticJSON);
+// let diagArry = [];
+// // const path = diagnosticJSON;
+
+// fs.readFile(diagnosticJSON, (err, data) => {
+//   if (err) {
+//     console.log(err + "readfile err");
+//   } else {
+//     diagArry = JSON.parse(data);
+//   }
+// }
+// )
 
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  console.log(diagArry);
+  res.json(diagArry);
 });
 
 // POST Route for a error logging
